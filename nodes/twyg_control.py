4d50f813c2ad20cdd76a344b5b152e8382c4fd29
Tim Warren <timlwarren@gmail.com>
2022-04-29 13:15:36 -0700

    working version

--- nodes/twyg_control.py
+++ nodes/twyg_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #modified from f-ponce magnotether_wind.py
 #by Ysabel Giraldo, Tim Warren 1.23.20
 #modified 11.19.21 to test writing to file
@@ -24,13 +24,13 @@ import os.path
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-import Queue
+import queue
 import time 
 from std_msgs.msg import Float64
 
 from magnotether.msg import MsgAngleData
 
-from find_fly_angle import find_fly_angle
+from find_fly_angle import find_fly_angle 
 
 class ImageConverter:  
 
@@ -40,9 +40,9 @@ class ImageConverter:
         self.bridge = CvBridge()
         rospy.on_shutdown(self.clean_up)
 
-        self.image_sub = rospy.Subscriber("/camera_array/cam0/image_raw",Image,self.callback)
+        self.image_sub = rospy.Subscriber("/pylon_camera_node/image_raw",Image,self.callback)
         self.angle_pub = rospy.Publisher('/angle_data', MsgAngleData, queue_size=10)
-
+        rospy.logwarn('subscribed')
 
         rospy.Subscriber('strip_led_info', StripLEDInfo, self.led_callback)
         
@@ -50,7 +50,7 @@ class ImageConverter:
         self.rotated_image_pub = rospy.Publisher('/rotated_image', Image, queue_size=10)
         self.contour_image_pub = rospy.Publisher('/contour_image', Image, queue_size=10)
 
-        self.queue = Queue.Queue()
+        self.queue = queue.Queue()
 
         self.threshold = 50
         self.mask_scale = 0.9
@@ -61,7 +61,7 @@ class ImageConverter:
         ##tw added
         ###You need to change self.data_path to a valid path for your filesystem
         ###e.g. '/home/giraldolab/data/'
-        self.data_path='/home/timothy/data/'
+        self.data_path='/home/flyranch/data/'
         self.file_name=self.data_path +time.strftime("%Y%m%d%H%M%S") + '.txt'
         self.file_handle = open(self.file_name,mode='w+')
     
@@ -79,7 +79,11 @@ class ImageConverter:
         cv2.destroyAllWindows()
 
     def callback(self,data): 
+        
         self.queue.put(data)
+        
+        rospy.logwarn('printing queue_size')
+        #rospy.logwarn(np.shape(tst))
 
     def run(self): 
 
@@ -89,22 +93,28 @@ class ImageConverter:
             new_image_list = []
 
             while True:
+                
+                
+
                 try:
                     ros_image = self.queue.get_nowait()
                     new_image_list.append(ros_image)
-                except Queue.Empty:
-                    print('error getting image')    
+                    rospy.logwarn('new image')
+                except queue.Empty:
+                    #print('error getting image')    
                     break
 
             for ros_image in new_image_list:
                 try:
                     cv_image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
                 except CvBridgeError as e:
+                    rospy.logwarn('error')
                     print(e)
 
                 self.frame_count += 1
                 cv_image_gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)  
-
+                rospy.logwarn('shape')
+                rospy.logwarn(np.shape(cv_image_gray))
                 angle_rad, angle_data = find_fly_angle(cv_image_gray, self.threshold, self.mask_scale)
 
                 angle_deg = np.rad2deg(angle_rad)
@@ -132,7 +142,8 @@ class ImageConverter:
             try:
                 self.write_data(cr_time,angle_deg)
             except:
-                self.write_data_with_led(cr_time,angle_deg)
+                tst=1
+                #print('error writing')
 
             if self.angle_data is not None:
                 cv2.imshow("raw image", self.angle_data['raw_image'])
@@ -141,14 +152,14 @@ class ImageConverter:
                 cv2.waitKey(1)
         self.file_handle.close()
     
-    def write_data(self,time,angle_deg)
+    def write_data(self,time,angle_deg):
         self.file_handle.write('%f %f\n'%(time,angle_deg))
         self.file_handle.flush()
-    def write_data_with_led(self,time,angle_deg)
+    def write_data_with_led(self,time,angle_deg):
         self.file_handle.write('%f %f %d\n'%(time,angle_deg, self.led_state.led_number))
         self.file_handle.flush()
-    def led_callback(self,led_info)
-    self.led_state=led_info
+    def led_callback(self,led_info):
+        self.led_state=led_info
 
 
 
